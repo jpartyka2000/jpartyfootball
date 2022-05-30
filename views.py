46ae0663ca91879168cb8bbce5a6fe88f8353c50
jpartyka2000 <jpartyka2000@gmail.com>
2022-05-30 12:57:13 -0600

    save newly created league form info - 1

--- views.py
+++ views.py
@@ -10,9 +10,10 @@ import os
 import re
 import pandas as pd
 import time
+import json
 
 from jpartyfb.forms import JPartyFBInputForm, CreateLeagueForm1
-from jpartyfb.models import DefaultTeams, City
+from jpartyfb.models import DefaultTeams, City, League, Division, Conference
 
 def build_choose_teams_html(number_of_divisions_select, number_of_teams_conf_select, number_of_teams_per_division):
 
@@ -69,17 +70,17 @@ def build_choose_teams_html(number_of_divisions_select, number_of_teams_conf_sel
         team_html_str += "<table cellpadding='5' width='100%' border='1'><tr>"
 
         # print out division header html - first easter division
-        team_html_str += "<td width='50%' align='center' style='background-color: #eeeeee;' id='td_division_eastern_" + str(conf_division_idx) + "' class='td_division'>"
+        team_html_str += "<td width='50%' align='center' style='background-color: blue;' id='td_division_eastern_" + str(conf_division_idx) + "' class='td_division'>"
         team_html_str += "<input type='text' id='Eastern_division_" + str(
             conf_division_idx) + "' name='eastern_division_" + str(
-            conf_division_idx) + "' class='division' maxlength='35' value='" + "Eastern " + conf_division_name + "' style='width: 220px; border: none; background: transparent; text-align:center; font-weight: bold;' />"
+            conf_division_idx) + "' class='division' maxlength='35' value='" + "Eastern " + conf_division_name + "' style='width: 220px; border: none; background: transparent; text-align:center; font-weight: bold; color: white;' />"
         team_html_str += "</td>"
 
         # now western division
-        team_html_str += "<td width='50%' align='center' style='background-color: #eeeeee;' id='td_division_western_" + str(conf_division_idx) + "' class='td_division'>"
+        team_html_str += "<td width='50%' align='center' style='background-color: red;' id='td_division_western_" + str(conf_division_idx) + "' class='td_division'>"
         team_html_str += "<input type='text' id='Western_division_" + str(
             conf_division_idx) + "' name='western_division_" + str(
-            conf_division_idx) + "' class='division' maxlength='35' value='" + "Western " + conf_division_name + "' style='width: 220px; border: none; background: transparent; text-align:center; font-weight: bold;' />"
+            conf_division_idx) + "' class='division' maxlength='35' value='" + "Western " + conf_division_name + "' style='width: 220px; border: none; background: transparent; text-align:center; font-weight: bold; color: white;' />"
         team_html_str += "</td>"
 
         # close division html row
@@ -198,6 +199,8 @@ def edit_league_settings(request):
 @csrf_exempt
 def process_create_league_form_1(request):
 
+    league_name = request.POST['league_name']
+
     #extract form information and pass it into the choose_teams page
     try:
         injury_checkbox = request.POST['injury_checkbox']
@@ -209,6 +212,11 @@ def process_create_league_form_1(request):
     except Exception:
         weather_checkbox = 'off'
 
+    try:
+        female_checkbox = request.POST['female_checkbox']
+    except Exception:
+        female_checkbox = 'off'
+
     number_of_playoff_teams_select = int(request.POST['number_of_playoff_teams_select'])
     number_of_weeks_select = int(request.POST['number_of_weeks_select'])
     number_of_teams_conf_select = int(request.POST['number_of_teams_conf_select'])
@@ -218,13 +226,13 @@ def process_create_league_form_1(request):
 
     team_html_str = build_choose_teams_html(number_of_divisions_select, number_of_teams_conf_select, number_of_teams_per_division)
 
-
-    #sdsddsds
     context = {}
 
     welcome_message = "Choose Your Teams"
+    context['league_name'] = league_name
     context['injury_mode'] = injury_checkbox
     context['weather_mode'] = weather_checkbox
+    context['female_mode'] = female_checkbox
     context['welcome_message'] = welcome_message
     context['number_of_playoff_teams'] = number_of_playoff_teams_select
     context['number_of_weeks'] = number_of_weeks_select
@@ -240,16 +248,112 @@ def process_create_league_form_1(request):
 def process_create_league_form_final(request):
 
     #extract form information and pass it into the choose_teams page
-    injury_hidden = request.POST['injury_hidden']
-    weather_hidden = request.POST['weather_hidden']
-    number_of_weeks_hidden = request.POST['number_of_weeks_hidden']
-    number_of_playoff_teams_hidden = request.POST['number_of_playoff_teams_hidden']
-    number_of_teams_conf_hidden = request.POST['number_of_teams_conf_hidden']
-    number_of_divisions_conf_hidden = request.POST['number_of_divisions_conf_hidden']
+    league_name = request.POST['league_name_hidden']
+    injury_setting = request.POST['injury_hidden']
+    weather_setting = request.POST['weather_hidden']
+    female_setting = request.POST['female_hidden']
+    number_of_weeks_setting = request.POST['number_of_weeks_hidden']
+    number_of_playoff_teams_setting = request.POST['number_of_playoff_teams_hidden']
+    number_of_teams_conf_setting = request.POST['number_of_teams_conf_hidden']
+    number_of_divisions_conf_setting = request.POST['number_of_divisions_conf_hidden']
 
-    context = {}
+    conference_name_list_str = request.POST['conference_name_list']
+    conference_name_list = json.loads(conference_name_list_str)
 
-    welcome_message = "Choose Your Teams"
-    context['welcome_message'] = welcome_message
+    division_name_list_str = request.POST['division_name_list']
+    division_name_list = json.loads(division_name_list_str)
 
-    return render(request, 'jpartyfb/choose_teams.html', context)
+    team_name_list_str = request.POST['team_name_list']
+    team_name_list = json.loads(team_name_list_str)
+
+    division_to_conference_dict_str = request.POST['division_to_conference_dict']
+    division_to_conference_dict = json.loads(division_to_conference_dict_str)
+
+    team_to_division_dict_str = request.POST['team_to_division_dict']
+    team_to_division_dict = json.loads(team_to_division_dict_str)
+
+    team_to_conference_dict_str = request.POST['team_to_conference_dict']
+    team_to_conference_dict = json.loads(team_to_conference_dict_str)
+
+    #create league abbrevation. If the league has >= 2 words, then use the first letter of each word
+    #if it has only one word, then use the first 3 letters of that word
+
+    league_name_parts_list = league_name.split()
+    league_name_abbrev_letter_list = []
+    league_name_abbrev_str = ""
+
+    if len(league_name_parts_list) >= 2:
+
+        for one_part in league_name_parts_list:
+            league_name_abbrev_letter_list.append(one_part[0])
+
+        league_name_abbrev_str = ''.join(league_name_abbrev_letter_list)
+
+    else:
+
+        league_name_abbrev_str = league_name[:3]
+
+    league_name_abbrev_str =  league_name_abbrev_str.upper()
+
+    #start inserting form data into database tables
+    #get latest League row
+    try:
+        league_id = int(
+            League.objects.using('xactly_dev').latest('id').id) + 1
+    except Exception:
+        league_id = 1
+
+    try:
+        League.objects.using("xactly_dev").create(id=league_id, name=league_name, abbreviation=league_name_abbrev_str, weather_setting=weather_setting, injury_setting=injury_setting, female_setting=female_setting)
+    except Exception:
+        return HttpResponse(-1)
+
+    #next, insert conference rows for this league
+    try:
+        conference_id = int(
+            Conference.objects.using('xactly_dev').latest('id').id) + 1
+    except Exception:
+        conference_id = 1
+
+    #we need this for proper insertions into the Division table
+    conference_name_to_id_dict = {}
+
+    for conference_idx, this_conference_name in enumerate(conference_name_list):
+
+        conference_name_to_id_dict[this_conference_name] = conference_id
+
+        try:
+            Conference.objects.using("xactly_dev").create(id=conference_id, conference_name=this_conference_name,
+                                                          league_id=league_id)
+
+        except Exception:
+            return HttpResponse(-2)
+
+        conference_id += 1
+
+    #now insert division rows for this league
+    try:
+        division_id = int(
+            Division.objects.using('xactly_dev').latest('id').id) + 1
+    except Exception:
+        division_id = 1
+
+    for division_idx, this_division_name in enumerate(division_name_list):
+
+        this_conference_name = division_to_conference_dict[this_division_name]
+        this_conference_id = conference_name_to_id_dict[this_conference_name]
+
+        try:
+            Division.objects.using("xactly_dev").create(id=division_id, division_name=this_division_name,
+                                                          conference_id=this_conference_id, first_season_id=-1, league_id=league_id)
+
+        except Exception:
+            return HttpResponse(-3)
+
+        division_id += 1
+
+
+    #status_and_variable_name_list = [result_status, variable_name_dict, unmatched_variables_dict]
+    #status_variable_names_json = json.dumps(status_and_variable_name_list)
+
+    return HttpResponse(1)
