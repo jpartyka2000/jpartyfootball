1fdf2f6afec13ea3991f192a62a63a0658e31230
jpartyka2000 <jpartyka2000@gmail.com>
2022-07-30 18:27:36 -0600

    add import statement for AssortedEnums

--- DraftUtils.py
+++ DraftUtils.py
@@ -6,6 +6,8 @@ from jpartyfb.models import PlayerSpecsQb, PlayerSpecsRb, PlayerSpecsWr, PlayerS
 
 from operator import itemgetter
 
+from AssortedEnums import PlayingStatus
+
 import json
 import random
 
